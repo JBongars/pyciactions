30b34e385f87b0aab50d0d881ae4d1ae57b27350
Julien Bongars <julien.bongars@airrlabs.com>
2023-06-07 03:50:40 +0000

    feat: init project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='pyactions',
+    version='1.0.0',
+    description='Declarative builder for Github Action Scripts',
+    author='Julien Bongars',
+    packages=['pyactions'],
+    install_requires=[],
+)
